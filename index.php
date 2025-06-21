daf4860961aa49cb47183fddba16e37825081a50
aponteloaizam <aponteloaizam@gmail.com>
2025-06-21 22:19:07 +0200

    Añadiendo ejercicio 3b

--- index.php
+++ index.php
@@ -40,6 +40,34 @@ echo "Imprimiendo operaciones con X y Y.\nSuma: " . ($x + $y). "\nResta: " . ($x
 echo "Imprimiendo operaciones con M y N.\nSuma: " . ($m + $n). "\nResta: " . ($m - $n) . "\nProducto: " . ($m * $n) . "\nMódulo: " . ($m % $n) . "\n\n";
 echo "Imprimiendo el doble de cada variable.\nX: " . ($x * 2) . "\nY: " . ($y * 2) . "\nM: " . ($m * 2) . "\nN: " . ($n * 2). "\n\n";
 echo "Imprimiendo la suma de todas las variables.\n" . ($x + $y + $m + $n) . "\n\n";
-echo "Imprimiendo el producto de todas las variables.\n" . ($x * $y * $m * $n) . "\n\n";
+echo "Imprimiendo el producto de todas las variables.\n" . ($x * $y * $m * $n);
+
+echo "\n\nEjercicio 3b\n\n";
+
+function calcular(int $var3a, int $var3b, string $operador): string {
+
+    switch ($operador) {
+        case "+":
+            $resultado = "El resultado de la suma es: " . ($var3a + $var3b);
+        break;
+        case "-":
+            $resultado = "El resultado de la resta es: " . ($var3a - $var3b);
+        break;
+        case "*":
+            $resultado = "El resultado de la multiplicación es: " . ($var3a * $var3b);
+        break;
+        case "/":
+            $resultado = "El resultado de la división es: " . ($var3a / $var3b);
+        break;
+        default:
+            $resultado = "El operador introducido es erróneo.";
+    }
+    return $resultado;
+}
+
+echo "Imprimiendo diferentes resultados de ejecutar la función calculadora.\n";
+echo calcular(2, 3, "*") . "\n";
+echo calcular(2, 3, "/") . "\n";
+echo calcular(2, 3, "&") . "\n";
 
 ?>
