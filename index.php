e1ab430de342dd62472ea22fb51ea0c1b543ce7c
aponteloaizam <aponteloaizam@gmail.com>
2025-06-21 18:06:15 +0200

    Añadiendo ejercicio 2

--- index.php
+++ index.php
@@ -15,4 +15,17 @@ define("NOMBRE","Ale");
 
 echo "Imprimiendo la constante en formato título.\n" . "<h1>" . NOMBRE . "</h1>";
 
+echo "\n\nEjercicio 2\n\n";
+
+$var2a = "Hello, World!";
+$var2b = "Este es el curso de PHP!";
+
+echo "Imprimiendo strings utilizando funciones.\n";
+
+echo "Original: " . $var2a . "\n";
+echo "Mayúsculas: " . strtoupper($var2a) . "\n";
+echo "Invertida: " . strrev($var2a) . "\n";
+echo "Concatenada: " . $var2a . " " . $var2b . "\n";
+echo "El tamaño del string original es de " . strlen($var2a) . " caracteres.\n";
+
 ?>
